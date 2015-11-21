33f6b3092455c8d854d7c6071d379115bc8939da
vehas <sky.48478@gmail.com>
2015-11-21 08:49:05 +0700

    init file from github.com/vehas/sax

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import {inc, dec} from './actions';
+require('./seedrandom.min.js');
+function gid(id ){
+  return  document.getElementById(id);
+}
+function gidh(id) {
+  return gid(id).innerHTML;
+}
+window.onload = function() {
+
+
+}
